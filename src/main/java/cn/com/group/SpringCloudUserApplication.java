cf0d520b05768059ec3c399834e6530bc6be73a0
cec <cec@cec-PC>
2019-03-06 18:53:31 +0800

    diyice

---
+++ src/main/java/cn/com/group/SpringCloudUserApplication.java
@@ -0,0 +1,13 @@
+package cn.com.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudUserApplication.class, args);
+	}
+
+}
