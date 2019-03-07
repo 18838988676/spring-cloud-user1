80b8502f5a75b3d707b160b1043a975e8af3d91e
cec <cec@cec-PC>
2019-03-07 18:04:11 +0800

    ribbon负载均衡

---
+++ src/main/java/cn/com/group/controller/UserController.java
@@ -0,0 +1,41 @@
+package cn.com.group.controller;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import cn.com.group.pojo.UserPo;
+
+@RestController
+public class UserController {
+	 private static Logger logger = Logger.getLogger(UserController.class);  
+	
+	 //服务发现客户端
+	 @Autowired
+	 private DiscoveryClient discoveryClient=null;
+	 
+	 //获取用户信息
+	 @GetMapping("/user/{id}")
+	 public UserPo getUserPo(@PathVariable("id") long id) {
+		 	ServiceInstance serviceInstance= discoveryClient.getInstances("wmcuser").get(0);
+		 	logger.info("serviceInstance.getUri():"+serviceInstance.getUri());
+		 	logger.info("serviceInstance.getUri():"+serviceInstance.getHost());
+		 	logger.info("serviceInstance.getUri():"+serviceInstance.getUri());
+		 	UserPo userPo=new UserPo();
+		 	userPo.setId(id);
+		 	userPo.setLevel((int) (id%2+1));
+		 	userPo.setUserName("user_name_"+id);
+		 	userPo.setNote("note_"+id);
+		 	return userPo;
+	 }
+	 
+	
+	
+
+}
---
+++ src/main/java/cn/com/group/pojo/UserPo.java
@@ -0,0 +1,46 @@
+package cn.com.group.pojo;
+
+import java.io.Serializable;
+
+public class UserPo implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String UserName;
+	private int level;
+	private String note;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+
+}
