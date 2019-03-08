24d8db06021b08e1752aae5757d7bbe5f8cf8647
cec <cec@cec-PC>
2019-03-08 17:23:16 +0800

    ribbon添加成功 ：访问http://localhost:9001/product/ribbon  即可通过产品，自动调用用户1或者用户2

--- src/main/java/cn/com/group/controller/UserController.java
+++ src/main/java/cn/com/group/controller/UserController.java
@@ -7,12 +7,14 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import cn.com.group.pojo.UserPo;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 	 private static Logger logger = Logger.getLogger(UserController.class);  
 	
@@ -23,7 +25,7 @@ public class UserController {
 	 //获取用户信息
 	 @GetMapping("/user/{id}")
 	 public UserPo getUserPo(@PathVariable("id") long id) {
-		 	ServiceInstance serviceInstance= discoveryClient.getInstances("wmcuser").get(0);
+		 	ServiceInstance serviceInstance= discoveryClient.getInstances("WMCUSER").get(0);
 		 	logger.info("serviceInstance.getUri():"+serviceInstance.getUri());
 		 	logger.info("serviceInstance.getUri():"+serviceInstance.getHost());
 		 	logger.info("serviceInstance.getUri():"+serviceInstance.getUri());
