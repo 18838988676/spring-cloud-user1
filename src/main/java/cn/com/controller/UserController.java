9f98ac512a2d5e62ab8eb39a961d9d87b00ad8ac
18838988676@163.com <ww@ww-PC>
2019-03-11 05:39:53 +0800

    添加feign中，crud

--- src/main/java/cn/com/controller/UserController.java
+++ src/main/java/cn/com/controller/UserController.java
@@ -1,18 +1,24 @@
 package cn.com.controller;
 
 
+import java.util.Map;
+
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
-import cn.com.pojo.UserPo;
+
+import cn.com.pojo.User;
 
 @RestController
 @RequestMapping("/user")
@@ -25,12 +31,12 @@ public class UserController {
 	 
 	 //获取用户信息
 	 @GetMapping("/user/{id}")
-	 public UserPo getUserPo(@PathVariable("id") long id) {
+	 public User getUserPo(@PathVariable("id") Integer id) {
 		 	ServiceInstance serviceInstance= discoveryClient.getInstances("WMCUSER").get(0);
 		 	logger.info("serviceInstance.getUri():"+serviceInstance.getUri());
 		 	logger.info("serviceInstance.getUri():"+serviceInstance.getHost());
 		 	logger.info("serviceInstance.getUri():"+serviceInstance.getUri());
-		 	UserPo userPo=new UserPo();
+		 	User userPo=new User();
 		 	userPo.setId(id);
 		 	userPo.setLevel((int) (id%2+1));
 		 	userPo.setUserName("user_name_"+id);
@@ -38,7 +44,14 @@ public class UserController {
 		 	return userPo;
 	 }
 	 
-	
+
+	 @PostMapping("/addUser")
+	 public Map<String, Object> addUser(@RequestBody User user){
+		 Map<String, Object> map=new HashedMap();
+		 map.put("msg1", "添加用户信息详情如下："+user+"，添加成功");
+		 return map;
+	 }
+	 
 	
 
 }
--- src/main/java/cn/com/pojo/User.java
+++ src/main/java/cn/com/pojo/User.java
@@ -2,20 +2,21 @@ package cn.com.pojo;
 
 import java.io.Serializable;
 
-public class UserPo implements Serializable{
+public class User implements Serializable{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private Long id;
+	private Integer id;
 	private String UserName;
 	private int level;
 	private String note;
-	public Long getId() {
+	
+	public Integer getId() {
 		return id;
 	}
-	public void setId(Long id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 	public String getUserName() {
@@ -38,6 +39,11 @@ public class UserPo implements Serializable{
 	}
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", UserName=" + UserName + ", level=" + level
+				+ ", note=" + note + "]";
 	}
 	
 	
